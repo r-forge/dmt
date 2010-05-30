a23aa967878f94c8874577f5f7886824cecc4fb6
lml (Leo Lahti) <leo.lahti@iki.fi>
2010-05-30 23:55:36 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dmt@53 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/dmt/R/AllClasses.R
@@ -0,0 +1,6 @@
+setClass("DependencyModel", representation(W = "list", phi = "list",
+                score = "numeric", loc = "numeric", geneName =
+                "character", windowSize = "numeric", chromosome =
+                "character", arm = "character", method = "character",
+                params = "list"))
+
