c1c7ec3b05d2e8e639b7132946951b846f10fda2
lml (Leo Lahti) <leo.lahti@iki.fi>
2010-05-31 07:49:00 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dmt@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dmt/R/z.projection.R
+++ pkg/dmt/R/z.projection.R
@@ -4,10 +4,10 @@ z.projection <- function(model,X,Y){
    index <- which(dimnames(X$data)[[1]] == getGeneName(model))
 
    # Check if model has only 1 variable from X data
-   if (nrow(getW(model)$X) == 1)
-     window <- sparse.window(X, Y, index, getWindowSize(model))
-   else
-     window <- fixed.window(X, Y, index, getWindowSize(model))
+   #if (nrow(getW(model)$X) == 1)
+   #  window <- sparse.window(X, Y, index, getWindowSize(model))
+   #else
+   #  window <- fixed.window(X, Y, index, getWindowSize(model))
 
    z <- z.expectation(model,X,Y)
    W <- getW(model)$total
@@ -22,6 +22,5 @@ z.projection <- function(model,X,Y){
    
    return(proj)
 
-   #barplot(t(proj),main="Probe data projection to first pca loading vector, zDim = 1 (pCCA)")
 }
 
