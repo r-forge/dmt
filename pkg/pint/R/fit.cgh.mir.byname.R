95c671a8c221b6b66a1f1463c4d9995a278d7c54
lml (Leo Lahti) <leo.lahti@iki.fi>
2010-05-31 08:29:18 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dmt@58 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pint/R/fit.cgh.mir.byname.R
+++
@@ -1,7 +0,0 @@
-fit.cgh.mir.byname <- function(X, Y, geneName, windowSize, ...){
-          
-          
-  index <- which(rownames(X$data) == geneName)
-  window <- sparse.window(X,Y,index,windowSize)
-  fit.dependency.model(window$X,window$Y,...)
-}
