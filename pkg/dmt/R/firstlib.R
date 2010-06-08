add3e6bdf4d05dba53d45e145c606365b42c8574
lml (Leo Lahti) <leo.lahti@iki.fi>
2010-06-08 21:27:05 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dmt@86 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dmt/R/firstlib.R
+++ pkg/dmt/R/firstlib.R
@@ -1,4 +1,4 @@
 .onLoad <- function(lib, pkg)
 {
-   cat('\ndmt Copyright (C) 2008-2010 Leo Lahti, Olli-Pekka Huovilainen, and Abhishek Tripathi.\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under GNU LGPL >=2.1, see the licensing terms for details.\n\n')
+   cat('\ndmt Copyright (C) 2008-2010 Leo Lahti, Olli-Pekka Huovilainen, and Abhishek Tripathi.\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under GNU GPL 2, see the licensing terms for details.\n\n')
 }
