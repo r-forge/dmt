8d27f2376c6a46baf1588f9560bd18811963830c
lml (Leo Lahti) <leo.lahti@iki.fi>
2010-06-24 13:25:38 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dmt@99 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/fit.cgh.mrna.byname.R
+++
@@ -1,7 +0,0 @@
-fit.cgh.mrna.byname <- function(X, Y, geneName, windowSize, ...){
-          
-          
-  index <- which(rownames(X$data) == geneName)
-  window <- fixed.window(X,Y,index,windowSize)
-  fit.dependency.model(window$X,window$Y, ...)
-}
