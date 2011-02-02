c7d31ed7427d7a83108a78da15f8b63efe7ac5ce
lml (Leo Lahti) <leo.lahti@iki.fi>
2011-02-02 13:52:34 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dmt@134 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/internals.R
+++ pkg/R/internals.R
@@ -1,20 +1,5 @@
 
 
- # the inverse of square root of a square matrix
-
-"SqrtInvMat" <-
- function(matrix)
-{
- x <- as.matrix(matrix)
-
- fac <- svd(x)
-
- res <-(fac$v %*% (diag(1/sqrt(fac$d),nrow = length(fac$d))) %*% t(fac$u))
-
- return(res)
-
-}
-
 
 
 solve.archambeau <- function (X, Y, Wx, Wy, btb.x, btb.y) {
@@ -113,4 +98,19 @@ function (A) {
 }
 
 
+
+ # the inverse of square root of a square matrix
+
+"SqrtInvMat" <- function(matrix)
+{
+
+ x <- as.matrix(matrix)
+
+ fac <- svd(x)
+
+ res <-(fac$v %*% (diag(1/sqrt(fac$d),nrow = length(fac$d))) %*% t(fac$u))
+
+ return(res)
+
+}
 
