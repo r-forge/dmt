92b143d8a297a5767d60cbab329eda290e5093a6
lml (Leo Lahti) <leo.lahti@iki.fi>
2011-04-12 21:32:15 +0000

    vignette updated
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dmt@182 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/firstlib.R
+++ pkg/R/firstlib.R
@@ -6,5 +6,6 @@
 
 .onLoad <- function(lib, pkg)
 {
-   cat('\ndmt Copyright (C) 2008-2011 Leo Lahti and Olli-Pekka Huovilainen.\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under FreeBSD, see the licensing terms for details.\n\n')
+   cat('\ndmt Copyright (C) 2008-2011 Leo Lahti and Olli-Pekka
+Huovilainen.\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the FreeBSD license.\n\n')
 }
